fa3a19bce6e2821a9a252dba180f8aa378a1e7f9
Rajan-Gupta365 <rajangupta98211@gmail.com>
2025-10-29 12:28:19 +0530

    Update apiKeyTesting.py

--- apiKeyTesiting.py
+++ apiKeyTesiting.py
@@ -1,10 +1,10 @@
 import google.generativeai as genai
 
-# ✅ Correct API key
-genai.configure(api_key="AIzaSyAV7nXYMei8jijfIPPfh8E17cDkFLXpnB0")
+
+genai.configure(api_k="use your own api key")
 
 try:
-    # ✅ Using the correct model from your list
+     
     model = genai.GenerativeModel("models/gemini-2.5-flash")
 
     response = model.generate_content("Who is Donald Trump?")
